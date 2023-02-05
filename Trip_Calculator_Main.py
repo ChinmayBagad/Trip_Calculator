f830866ee5bf7f1fa280f8ef798cf47aad5f558d
ChinmayBagad <117026366+ChinmayBagad@users.noreply.github.com>
2023-02-05 12:01:24 +0530

    Sloved the error from previous documentation

--- Trip_Calculator_Main.py
+++ Trip_Calculator_Main.py
@@ -1,53 +1,50 @@
-name_list=[]
- 
+name_list = []
+
 while True:
-    name=input("Enter the names (or 'q' to quit):-")
+    name = input("Enter the names (or 'q' to quit):-")
     if name == "q":
         break
     name_list.append(name)
     
-
-x=len(name_list)
+x = len(name_list)
 money_spend = []
 
 print(name_list)
+
 for i in range (x):
-    sel=int(input("Enter the Number of the person from the List:- "))
-    if sel >= 0 or sel <x:
-         z=name_list[sel]
-         spend = []
-    else:
-        print("Plaese Enter a Valide Number from the list")  
-
     while True:
-            exp=input("Enter the Amount spend by "+ z + "(or 'q' to exit):-" )
-            if exp == 'q':
-               break
-
-            exp=float(exp)
+        try:
+            sel = int(input("Enter the Number of the person from the List:- "))
+            if sel >= 0 and sel < x:
+                name = name_list[sel]
+                spend = []
+                break
+            else:
+                print("ERROR CODE ( 0 ):- Enter a Valide Number from the list.")
+        except (ValueError):
+            print("ERROR CODE ( 0 ):- Enter a Valide Number from the list.")
+    
+    while True:
+        exp = input("Enter the Amount spend by "+ name + " (or 'q' to exit):- " )
+        if exp == 'q':
+            break
+        try:
+            exp = float(exp)
             spend.append(exp)
-            total=sum(spend)
-
+            total = sum(spend)
+            break
+        except (ValueError):
+            print("ERROR CODE ( 1 ) :- Enter a Valide Number.")
     
-
-
     money_spend.append(total)
-    ultra=sum(money_spend)
-    
-    # Distribution Part
-per_person=ultra/x
-print("Total Money Spend = ",ultra)
-print("Per Person Cost = ",per_person) 
-        
 
-        
+ultra = sum(money_spend)
 
+# Distribution Part
+per_person = ultra / x
+print("Total Money Spend = ", ultra)
+print("Per Person Cost = ", per_person)
 
-
-
-
-
-        
     
         
     
